8526bbc2e7e627f9b66ef7aa0fc331b68d7abc6a
igor5544 <igor5544@bk.ru>
2020-06-10 19:38:12 +0500

    добавляет хедер

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,10 @@
 'use strict';
 
 (function () {
+
+  var headerElement = document.querySelector('.header');
+  var toggleBtnElement = headerElement.querySelector('.header__toggle-btn');
+
+  toggleBtnElement.classList.remove('header__toggle-btn--nojs');
+
 })();
